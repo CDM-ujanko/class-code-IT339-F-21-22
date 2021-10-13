d4fa33e9576f41b3ce350e506ba10b40c81adaff
Uros Jankovic <bug24b@gmail.com>
2021-10-13 18:39:33 +0200

    Week 5w class code.

--- week5/routes/notes.mjs
+++ week5/routes/notes.mjs
@@ -7,20 +7,52 @@ let notes = new InMemoryNotesStore();
 /* GET home page. */
 router.get('/', async(req, resp, next) => {
   resp.render('notes', {
-    title: 'Notes',
+    title: 'My Notes',
     noteKeys: await notes.keyList()
   });
 });
 
-router.get('/add', (req, resp, next) => {
+router.get('/add', async (req, resp, next) => {
   if (req.query.key && req.query.title && req.query.body) {
-    notes.create(req.query.key, req.query.title, req.query.body);
+    await notes.create(req.query.key, req.query.title, req.query.body);
     resp.redirect('/notes');
   } else {
     resp.render('edit-note', {
       title: 'Create Note'
     });
   }
+});
+
+router.get('/edit/:key', async (req, resp, next) => {
+  let note = await notes.read(req.params.key);
+
+  if (!note) {
+    next(new Error(`No such key ${req.params.key}`));
+    return;
+  }
+
+  if (req.query.title && req.query.body) {
+    await notes.update(note.key, req.query.title, req.query.body);
+    resp.redirect('/notes');
+  } else {
+    resp.render('edit-note', {
+      title: 'Edit Note',
+      note: note,
+      edit: true
+    });
+  }
+});
+
+router.get('/delete/:key', async (req, resp, next) => {
+  let note = await notes.read(req.params.key);
+
+  if (!note) {
+    next(new Error(`No such key ${req.params.key}`));
+    return;
+  }
+
+  await notes.destroy(note.key);
+  resp.redirect('/notes');
 });
 
 export default router;
